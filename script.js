df1a16940374f6a3681ae25397fb72b9a7cbbe21
Thomas S <thomas.swanson107@gmail.com>
2023-04-16 12:19:29 -0700

    Add gui functionality

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let books = [];
+const titleInput = document.getElementById('title-input');
+const dateInput = document.getElementById('date-input');
+const authorInput = document.getElementById('author-input');
+const submitError = document.getElementById('submit-error');
 
 function Book(title, date, author) {
     this.title = title;
@@ -16,18 +20,26 @@ books.push(twilight);
 books.push(twilightTwo);
 
 function addToBooks() {
-    let newTitle = prompt('Enter a book title');
-    let newDate = prompt('Enter a date (must be a valid integer)');
-    let newAuthor = prompt('Enter author name');
+    let newTitle = titleInput.value;
+    let newDate = dateInput.value;
+    let newAuthor = authorInput.value;
 
     const newBook = new Book(newTitle, parseInt(newDate), newAuthor);
     books.push(newBook);
-    
 }
 
 console.table(books);
 
 
 document.getElementById('submit-book').addEventListener('click', function() {
-    
+    if (titleInput.value === '' || dateInput.value === '' || authorInput.value === '') {
+        submitError.textContent = 'Error. One or more fields are empty';
+    } else {
+        addToBooks();
+        titleInput.value = '';
+        dateInput.value = '';
+        authorInput.value = '';
+        submitError.textContent = 'Book submitted successfully';
+        console.table(books);
+    }
 })
